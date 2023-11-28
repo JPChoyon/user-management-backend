47295802893ed7ff9853cad65d98c98a820bf4e3
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-11-28 23:50:38 +0600

    added jwt in the local storage

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const express = require('express');
-const cors = require('cors');
-const app = express()
-const jwt = require('jsonwebtoken')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId, } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 const port = process.env.PORT || 5000;
 
-// middle ware 
-app.use(cors())
-app.use(express.json())
-
-const verifyToken = (req, res, next) => {
-  if (!req.headers.Authorization) {
-    return res.status(401).send({ message: 'unauthorized access' })
-  }
-  const token = req.headers.Authorization.split(' ')[1]
-  jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: 'unauthorized access' })
-    }
-    req.decoded = decoded
-    next()
-  })
-}
+require("dotenv").config()
 
 
 // mongodb connect 
@@ -47,13 +33,39 @@ async function run() {
 
     await client.db("admin").command({ ping: 1 });
     const userCollection = client.db("userManagement").collection("users");
-    // json web token 
-    app.post('/jwt', async (req, res) => {
-      const user = req.body;
 
-      const token = jwt.sign(user, process.env.SECRET, { expiresIn: '1h' })
-      res.send({ token })
+    // jwt authinacation
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET, {
+        expiresIn: "100d",
+      });
+      res.send(token);
+    });
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization;
+      jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     })
+
+
 
 
     app.get('/users', verifyToken, async (req, res) => {
